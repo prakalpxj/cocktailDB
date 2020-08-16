85333c7774fe52f7e915d732d9a3473a5f759200
prakalpxj <prakalp.jain.05@gmail.com>
2020-08-17 00:20:18 +0530

    last commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                This is a database for finding recipe of your favourite cocktails from all over the world
+                so you can make them at your place by yourself.
+      </p>
+        </section>
+    );
+}
